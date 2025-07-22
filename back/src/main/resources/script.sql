031bbbf20e645839b98bb3b1342d66a71c0afdf2
Hugo-Robert-1 <robert.hugo.051@gmail.com>
2025-07-22 02:17:15 +0200

    chore: script sql pour créer la base de données du projet avec des données

---
+++ back/src/main/resources/script.sql
@@ -0,0 +1,58 @@
+CREATE TABLE `SUBJECTS` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `description` VARCHAR(2000),
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `USERS` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(255),
+  `email` VARCHAR(255),
+  `password` VARCHAR(255),
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `POSTS` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `title` VARCHAR(255),
+  `content` VARCHAR(2000),
+  `subject_id` INT,
+  `user_id` INT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `COMMENTS` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `content` VARCHAR(2000),
+  `user_id` INT,
+  `post_id` INT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `SUBSCRIPTION` (
+  `user_id` INT, 
+  `subject_id` INT,
+  PRIMARY KEY (`user_id`, `subject_id`)
+);
+
+ALTER TABLE `POSTS` ADD FOREIGN KEY (`subject_id`) REFERENCES `SUBJECTS` (`id`);
+ALTER TABLE `POSTS` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+
+ALTER TABLE `SUBSCRIPTION` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+ALTER TABLE `SUBSCRIPTION` ADD FOREIGN KEY (`subject_id`) REFERENCES `SUBJECTS` (`id`);
+
+ALTER TABLE `COMMENTS` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+ALTER TABLE `COMMENTS` ADD FOREIGN KEY (`post_id`) REFERENCES `POSTS` (`id`);
+
+INSERT INTO `SUBJECTS` (`name`, `description`)
+VALUES 
+  ('Cybersécurité', 'Thématique de la sécurité informatique'),
+  ('Gestion de projet', 'Thématique sur la gestion de projet informatique'),
+  ('Réseaux', 'Thématique sur les réseaux'),
+  ('Infrastucture', 'Thématique sur les infrastructures'),
+  ('Nouvelles technologies', 'Thématique abordant les nouveautés dans le domaine de la tech');
