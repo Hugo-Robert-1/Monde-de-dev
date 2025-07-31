cbb016ec29dca8b9b19352e3200f927d15d36905
Hugo-Robert-1 <robert.hugo.051@gmail.com>
2025-07-31 07:54:57 +0200

    fix: back - config + ajout gestion cors

---
+++ back/src/main/java/com/orion/mdd/mddapi/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.orion.mdd.mddapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // toutes les routes
+						.allowedOrigins("http://localhost:4200") // autorise ce frontend
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+}
