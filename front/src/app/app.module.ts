9f27e7142533cbfd063af4d3a1e99ff5c66c0047
Hugo-Robert-1 <robert.hugo.051@gmail.com>
2025-08-02 07:39:09 +0200

    lint: suppression code/import inutilisé

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { ReactiveFormsModule } from '@angular/forms';
--- front/src/app/features/sessions/interfaces/sessionInformation.interface.ts
+++ front/src/app/features/sessions/interfaces/sessionInformation.interface.ts
@@ -1,7 +1,3 @@
 export interface SessionInformation {
   accessToken: string;
-  type: string;
-  id: number;
-  email: string;
-  username: string;
 }
